689b43539a8e94ebac806ceb61d659f53bdf68da
EdilsonRohden <edilsonrohden11@gmail.com>
2020-12-08 21:48:11 -0300

    Bugfix render

--- src/App.js
+++ src/App.js
@@ -1,167 +1,174 @@
+import { useState } from 'react';
 import './App.css';
 import init from "./init"
 
-const state = {}
-const cache = []
-const ram = []
-const est = {
-  n_acessos: 0,
-  n_acertos: 0,
-  n_faltas: 0,
-  n_leituras: 0,
-  n_escritas: 0,
-  n_acertos_leitura: 0,
-  n_acertos_escrita: 0,
-  n_faltas_leitura: 0,
-  n_faltas_escrita: 0
-}
-init(cache, ram)
-
-function read(address){
-  console.log("Before Reading")
-  console.log(cache)
-  console.log(ram)
-  console.log(est)
-  const result = getCellFromCache(address)
-  console.log("After Reading")
-  console.log(cache)
-  console.log(ram)
-  console.log(est)
-  return result
-}
-
-
-function write(address, value){
-  console.log("Before Writting")
-  console.log(cache)
-  console.log(ram)
-  console.log(est)
-  writeCellToCache(address, value)
-  console.log("After Writting")
-  console.log(cache)
-  console.log(ram)
-  console.log(est)
-}
-
-function getCellFromCache(address) {
-  console.log("getting from cache")
-  const quadro = getQuadro(address)
-
-  if(quadro.tag === getTag(address)){
-    console.log("hit")
-    hitOnRead()
-    const cell = getCellDecimalPosition(address)
-    return quadro.cells[cell]
-  }
-  console.log("miss")
-  missOnRead()
-  const block =  getBlockFromRAM(address);
-  writeBlockToCache(address, block)
-  return block[getCellDecimalPosition(address)]
-}
-
-function writeCellToCache(address, value){
-  console.log("writting to cache")
-  const quadro = getQuadro(address)
-  if(quadro.tag === getTag(address)){
-    console.log("hit")
-    hitOnWrite()
-    const cell = getCellDecimalPosition(address)
-    quadro.cells[cell] = value
-    writeBlockToRAM(address, quadro.cells)
-    return
-  }
-  console.log("miss")
-  missOnWrite()
-  getFromRamAndWrite();
-  function getFromRamAndWrite() {
-    const block = getBlockFromRAM(address);
-    writeBlockToCache(address, block);
-    const quadro = getQuadro(address);
-    const cell = getCellDecimalPosition(address);
-    quadro.cells[cell] = value;
-    writeBlockToRAM(address, quadro.cells);
-  }
-}
-
-function writeBlockToCache(address, block){
-  console.log("writting to cache")
-  const quadro = getQuadro(address)
-  quadro.cells = block
-  quadro.tag = getTag(address)
-}
-
-function getBlockFromRAM(address) {
-  est.n_acessos++
-  const decimalAddress = getDecimalAddress(address)
-  let beginBlock = parseInt(decimalAddress/cache.length) * cache.length
-  const endBlock = beginBlock + cache.length
-  const block = []
-  while(beginBlock < endBlock){
-    block.push(ram[beginBlock])
-    beginBlock++
-  }
-  return block;
-}
-
-function writeBlockToRAM(address, block){
-  est.n_acessos++
-  const decimalAddress = getDecimalAddress(address)
-  let beginBlock = parseInt(decimalAddress/cache.length) * cache.length
-  block.forEach(cell => {
-    ram[beginBlock] = cell
-    beginBlock++
-  });
-}
-
-function getTag(address){
-  return address.substring(0, 3)
-}
-
-function getCellDecimalPosition(address){
-  return parseInt(address.substring(7, 11), 2)
-}
-
-function getDecimalAddress(address){
-  return parseInt(address, 2)
-}
-
-function getQuadro(address) {
-  const decimalAddress = getDecimalAddress(address)
-  const quadroDestino = decimalAddress % cache.length
-  return cache[quadroDestino]
-}
-
-function hitOnRead(){
-  est.n_acertos++
-  est.n_leituras++
-  est.n_acertos_leitura++
-}
-function missOnRead(){
-  est.n_faltas_leitura++
-  est.n_leituras++
-  est.n_faltas++
-}
-
-function hitOnWrite(){
-  est.n_acertos_escrita++
-  est.n_escritas++
-  est.n_acertos++
-}
-function missOnWrite(){
-  est.n_faltas_escrita++
-  est.n_escritas++
-  est.n_faltas++
-}
-function createBinaryString (index) {
-  let binary = index.toString(2) 
-  while(binary.length < 11){
-    binary = "0" + binary
-  }
-  return binary
-}
 
 function App() {
+  const [address, setAddress] = useState("")
+  const [value, setValue] = useState("")
+  const [cache, setCache] = useState([])
+  const [ram, setRam] = useState([])
+  const [est, setEst] = useState({
+    n_acessos: 0,
+    n_acertos: 0,
+    n_faltas: 0,
+    n_leituras: 0,
+    n_escritas: 0,
+    n_acertos_leitura: 0,
+    n_acertos_escrita: 0,
+    n_faltas_leitura: 0,
+    n_faltas_escrita: 0
+})
+  
+  init(cache, ram, est)
+  
+  function read(address){
+    console.log("Before Reading")
+    console.log(cache)
+    console.log(ram)
+    console.log(est)
+    const result = getCellFromCache(address)
+    setCache([...cache])
+    setRam([...ram])
+    console.log("After Reading")
+    console.log(cache)
+    console.log(ram)
+    console.log(est)
+    return result
+  }
+  
+  
+  function write(address, value){
+    console.log("Before Writting")
+    console.log(cache)
+    console.log(ram)
+    console.log(est)
+    writeCellToCache(address, value)
+    setCache([...cache])
+    setRam([...ram])
+    console.log("After Writting")
+    console.log(cache)
+    console.log(ram)
+    console.log(est)
+  }
+  
+  function getCellFromCache(address) {
+    console.log("getting from cache")
+    const quadro = getQuadro(address)
+  
+    if(quadro.tag === getTag(address)){
+      console.log("hit")
+      hitOnRead()
+      const cell = getCellDecimalPosition(address)
+      return quadro.cells[cell]
+    }
+    console.log("miss")
+    missOnRead()
+    const block =  getBlockFromRAM(address);
+    writeBlockToCache(address, block)
+    return block[getCellDecimalPosition(address)]
+  }
+  
+  function writeCellToCache(address, value){
+    console.log("writting to cache")
+    const quadro = getQuadro(address)
+    if(quadro.tag === getTag(address)){
+      console.log("hit")
+      hitOnWrite()
+      const cell = getCellDecimalPosition(address)
+      quadro.cells[cell] = value
+      writeBlockToRAM(address, quadro.cells)
+      return
+    }
+    console.log("miss")
+    missOnWrite()
+    getFromRamAndWrite();
+    function getFromRamAndWrite() {
+      const block = getBlockFromRAM(address);
+      writeBlockToCache(address, block);
+      const quadro = getQuadro(address);
+      const cell = getCellDecimalPosition(address);
+      quadro.cells[cell] = value;
+      writeBlockToRAM(address, quadro.cells);
+    }
+  }
+  
+  function writeBlockToCache(address, block){
+    console.log("writting to cache")
+    const quadro = getQuadro(address)
+    quadro.cells = block
+    quadro.tag = getTag(address)
+  }
+  
+  function getBlockFromRAM(address) {
+    est.n_acessos++
+    const decimalAddress = getDecimalAddress(address)
+    let beginBlock = parseInt(decimalAddress/cache.length) * cache.length
+    const endBlock = beginBlock + cache.length
+    const block = []
+    while(beginBlock < endBlock){
+      block.push(ram[beginBlock])
+      beginBlock++
+    }
+    return block;
+  }
+  
+  function writeBlockToRAM(address, block){
+    est.n_acessos++
+    const decimalAddress = getDecimalAddress(address)
+    let beginBlock = parseInt(decimalAddress/cache.length) * cache.length
+    block.forEach(cell => {
+      ram[beginBlock] = cell
+      beginBlock++
+    });
+  }
+  
+  function getTag(address){
+    return address.substring(0, 3)
+  }
+  
+  function getCellDecimalPosition(address){
+    return parseInt(address.substring(7, 11), 2)
+  }
+  
+  function getDecimalAddress(address){
+    return parseInt(address, 2)
+  }
+  
+  function getQuadro(address) {
+    const decimalAddress = getDecimalAddress(address)
+    const quadroDestino = decimalAddress % cache.length
+    return cache[quadroDestino]
+  }
+  
+  function hitOnRead(){
+    est.n_acertos++
+    est.n_leituras++
+    est.n_acertos_leitura++
+  }
+  function missOnRead(){
+    est.n_faltas_leitura++
+    est.n_leituras++
+    est.n_faltas++
+  }
+  
+  function hitOnWrite(){
+    est.n_acertos_escrita++
+    est.n_escritas++
+    est.n_acertos++
+  }
+  function missOnWrite(){
+    est.n_faltas_escrita++
+    est.n_escritas++
+    est.n_faltas++
+  }
+  function createBinaryString (index, length) {
+    let binary = index.toString(2) 
+    while(binary.length < length){
+      binary = "0" + binary
+    }
+    return binary
+  }
   return (
     <div className="App">
 
@@ -180,7 +187,7 @@ function App() {
           <tbody>
             {cache.map((value, index) => (
                 <tr key={index}>
-                  <td key={index+"quadro"}>Quadro-{index}</td>
+                  <td key={index+"quadro"}>{value.tag}</td>
                   {value.cells.map((element, inde) => (
                     <td key={inde+"cell"}>{element}</td>
                   ))}
@@ -191,14 +198,14 @@ function App() {
       </div>
       <div className="readWrite">
         <input placeholder="Endereço"
-          type="text" value={state.address} onChange={(event)=>{state.address = event.target.value}}></input>
+          type="text" value={address} onChange={(event)=>{setAddress(event.target.value)}}></input>
         <input placeholder="Valor" 
-          type="text" value={state.value} onChange={(event)=>{state.value = event.target.value}}></input>
+          type="text" value={value} onChange={(event)=>{setValue(event.target.value)}}></input>
         <button onClick={()=> {
-          write(state.address, state.value)
+          write(address, value)
         }}>Write</button>
         <button onClick={()=> {
-          state.value = read(state.address)
+          setValue(read(address))
         }}>read</button>
       </div>
       <div className="estatistics">
@@ -233,7 +240,7 @@ function App() {
           <tbody>
             {ram.map((value, index) => (
                 <tr key={index+"celula"}>
-                  <td key={index+"binary"}>{createBinaryString(index)}</td>
+                  <td key={index+"binary"}>{createBinaryString(index, 11)}</td>
                   <td key={index+"binaryValue"}>{value}</td>
                 </tr>
             ))}
--- src/init.js
+++ src/init.js
@@ -1,14 +1,28 @@
-export default (cache, ram) => {
-    for (let i = 0; i < 16; i++) {
-        cache.push({
-            tag: "",
-            cells: ["","","","","","","","","","","","","","","",""]
-        });        
-    }
-    for (let i = 0; i < 2048; i++) {
-        ram.push(Math.random().toString(36).substring(5,4));
-    }
+export default (cache, ram, est) => {
+    if(cache?.length !== 16){
+        for (let i = 0; i < 16; i++) {
+            cache.push({
+                tag: "---",
+                cells: ["","","","","","","","","","","","","","","",""]
+            });        
+        }
+        for (let i = 0; i < 2048; i++) {
+            ram.push(Math.random().toString(36).substring(5,4));
+        }
 
+        est = {
+            n_acessos: 0,
+            n_acertos: 0,
+            n_faltas: 0,
+            n_leituras: 0,
+            n_escritas: 0,
+            n_acertos_leitura: 0,
+            n_acertos_escrita: 0,
+            n_faltas_leitura: 0,
+            n_faltas_escrita: 0
+        }
+    }
     console.log(cache)
     console.log(ram)
+    console.log(est)
 }
